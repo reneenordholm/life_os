405defd94fffe9f4fc8932983a882cbbc35c1a13
Renee Nordholm <juliereneehayes@gmail.com>
2026-05-06 07:21:59 -0700

    add automatic document ingestion on create and update

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,4 +2,22 @@ class Document < ApplicationRecord
   has_many :document_chunks, dependent: :destroy
   has_many :document_entities, dependent: :destroy
   has_many :entities, through: :document_entities
+
+  after_save :mark_for_ingestion
+  after_commit :ingest_if_needed, on: [ :create, :update ]
+
+  private
+
+  def mark_for_ingestion
+    @needs_ingestion = saved_change_to_content? ||
+                       saved_change_to_title? ||
+                       saved_change_to_doc_type? ||
+                       saved_change_to_metadata?
+  end
+
+  def ingest_if_needed
+    return unless @needs_ingestion
+
+    DocumentIngestionService.new(self).call
+  end
 end
--- app/services/daily_log_service.rb
+++ app/services/daily_log_service.rb
@@ -13,8 +13,6 @@ class DailyLogService
       }
     )
 
-    DocumentIngestionService.new(doc).call
-
     doc
   end
 
