9f5136197024262a803ef51193bbf728b01a15f0
Renee Nordholm <juliereneehayes@gmail.com>
2026-05-06 07:51:10 -0700

    update ingestion to be self-contained

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,17 +3,12 @@ class Document < ApplicationRecord
   has_many :document_entities, dependent: :destroy
   has_many :entities, through: :document_entities
 
-  after_save :mark_for_ingestion
   after_commit :ingest_if_needed, on: [ :create, :update ]
 
   private
 
-  def mark_for_ingestion
-    @needs_ingestion = saved_change_to_content?
-  end
-
   def ingest_if_needed
-    return unless @needs_ingestion
+    return unless previous_changes.key?("content")
 
     DocumentIngestionService.new(self).call
   end
