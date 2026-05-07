219ce17096988092e23488d1412394641e2d39e8
Renee Nordholm <juliereneehayes@gmail.com>
2026-05-06 20:35:10 -0700

    fix embedding stub not running in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,12 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+# Stub embeddings once per worker
+Rails.application.config.after_initialize do
+  EmbeddingService.define_singleton_method(:embed) do |_text|
+    Array.new(1536, 0.0)
+  end
+end
 module ActiveSupport
   class TestCase
     include ActiveSupport::Testing::TimeHelpers
@@ -9,18 +15,11 @@ module ActiveSupport
     # Run tests in parallel
     parallelize(workers: :number_of_processors)
 
-    # ❌ no fixtures
+    # no fixtures
     # fixtures :all
 
     teardown do
       travel_back
     end
-  end
-end
-
-# 👉 Stub embeddings once per worker (safe with process-based parallelization)
-Rails.application.config.after_initialize do
-  EmbeddingService.define_singleton_method(:embed) do |_text|
-    Array.new(1536, 0.0)
   end
 end
