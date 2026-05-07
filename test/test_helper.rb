805b54636ba25dd885b7fa834b71df668df77c57
Renee Nordholm <juliereneehayes@gmail.com>
2026-05-06 19:24:22 -0700

    disable default fixtures in test suite

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,7 +10,9 @@ module ActiveSupport
     parallelize(workers: :number_of_processors)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
+
+    # ❌ disable fixtures
+    # fixtures :all
 
     setup do
       @original_embed = EmbeddingService.method(:embed)
