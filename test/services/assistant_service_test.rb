85b48634dd57478c04d525063ef036adc203dc51
Renee Nordholm <juliereneehayes@gmail.com>
2026-05-10 19:27:25 -0700

    test update to better verify log return

--- test/services/assistant_service_test.rb
+++ test/services/assistant_service_test.rb
@@ -270,7 +270,7 @@ class AssistantServiceTest < ActiveSupport::TestCase
       end
 
       assert_equal "stubbed response", service.call
-      assert_includes captured_context, "Spent time with #{entity.name}"
+      assert_includes captured_context, "Spent time with Mom"
       assert_not_includes captured_context, "Worked all day"
     end
   end
