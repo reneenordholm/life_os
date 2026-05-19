79c50b61c7076abed509c2bc361dcaa78b3e2978
Renee Nordholm <juliereneehayes@gmail.com>
2026-05-19 07:22:38 -0700

    Polish Today dashboard layout

--- app/controllers/today_controller.rb
+++ app/controllers/today_controller.rb
@@ -2,6 +2,16 @@ class TodayController < ApplicationController
   def index
     today = Date.current.iso8601
 
+    @greeting =
+      case Time.current.hour
+      when 5...12
+        "Good morning"
+      when 12...17
+        "Good afternoon"
+      else
+        "Good evening"
+      end
+
     @daily_log = Document
       .where(doc_type: "daily_log")
       .where("metadata ->> 'date' = ?", today)
